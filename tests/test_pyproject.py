32bde19b75090ddc11443250442e51def5b3b40b
Martin Herrmann <martin@famherrmann.de>
2025-02-03 21:42:50 +0100

    Add unit test

---
+++ tests/test_pyproject.py
@@ -0,0 +1,11 @@
+from payne import Pyproject
+
+class TestPyproject:
+    def test_name(self):
+        p = Pyproject({
+            "project": {
+                "name": "foo",
+            },
+        })
+
+        assert p.name() == "foo"
