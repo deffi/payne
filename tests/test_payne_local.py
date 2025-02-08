431cabc721a1c9fca56a80df6b8cb388c1608fe2
Martin Herrmann <martin@famherrmann.de>
2025-02-08 20:38:32 +0100

    Add remote installation test

--- tests/test_payne_local.py
+++ tests/test_payne_local.py
@@ -72,6 +72,66 @@ class TestPayneLocal:
                 "This is foo 1.3.0\nThis is bar 1.2.1\nThis is baz 1.1.1\n", "")
             assert process_output([bin_dir / "foo-1.3.1"]) == (
                 "This is foo 1.3.1\nThis is bar 1.2.0\nThis is baz 1.1.1\n", "")
+            assert process_output([bin_dir / "foo-1.3.2"]) == (
+                "This is foo 1.3.2\nThis is bar 1.2.0\nThis is baz 1.1.0\n", "")
+
+            # Uninstall foo 1.3.0
+            payne.uninstall("foo", "1.3.0")
+            assert self.installed_apps(apps_dir) == {"foo": {"1.3.1", "1.3.2"}}
+            assert self.installed_scripts(bin_dir) == {"foo-1.3.1", "foo-1.3.2"}
+            self.assert_app_valid(apps_dir, "foo", "1.3.1")
+            self.assert_app_valid(apps_dir, "foo", "1.3.2")
+
+            # Uninstall foo 1.3.1
+            payne.uninstall("foo", "1.3.1")
+            assert self.installed_apps(apps_dir) == {"foo": {"1.3.2"}}
+            assert self.installed_scripts(bin_dir) == {"foo-1.3.2"}
+            self.assert_app_valid(apps_dir, "foo", "1.3.2")
+
+            # Uninstall foo 1.3.2
+            payne.uninstall("foo", "1.3.2")
+            assert self.installed_apps(apps_dir) == {}
+            assert self.installed_scripts(bin_dir) == set()
+
+    def test_install_from_remote_uninstall(self):
+        with TemporaryDirectory() as temp_dir:
+            temp_dir = Path(temp_dir)
+            apps_dir = temp_dir / "apps"
+            bin_dir = temp_dir / "bin"
+
+            payne = Payne(apps_dir, bin_dir)
+            # TODO this variable should be independent of uv
+            os.environ["UV_INDEX"] = "payne_test_data=http://localhost:8000/payne_test_data"
+
+            # Install foo 1.3.0
+            payne.install_from_remote("foo", "1.3.0")
+            assert self.installed_apps(apps_dir) == {"foo": {"1.3.0"}}
+            assert self.installed_scripts(bin_dir) == {"foo-1.3.0"}
+            self.assert_app_valid(apps_dir, "foo", "1.3.0")
+
+            # Install foo 1.3.1
+            payne.install_from_remote("foo", "1.3.1")
+            assert self.installed_apps(apps_dir) == {"foo": {"1.3.0", "1.3.1"}}
+            assert self.installed_scripts(bin_dir) == {"foo-1.3.0", "foo-1.3.1"}
+            self.assert_app_valid(apps_dir, "foo", "1.3.0")
+            self.assert_app_valid(apps_dir, "foo", "1.3.1")
+
+            # Install foo 1.3.2
+            payne.install_from_remote("foo", "1.3.2")
+            assert self.installed_apps(apps_dir) == {"foo": {"1.3.0", "1.3.1", "1.3.2"}}
+            assert self.installed_scripts(bin_dir) == {"foo-1.3.0", "foo-1.3.1", "foo-1.3.2"}
+            self.assert_app_valid(apps_dir, "foo", "1.3.0")
+            self.assert_app_valid(apps_dir, "foo", "1.3.1")
+            self.assert_app_valid(apps_dir, "foo", "1.3.2")
+
+            # Run the scripts
+            # Windows will automatically use the .exe
+            assert process_output([bin_dir / "foo-1.3.0"]) == (
+                "This is foo 1.3.0\nThis is bar 1.2.1\nThis is baz 1.1.1\n", "")
+            assert process_output([bin_dir / "foo-1.3.1"]) == (
+                "This is foo 1.3.1\nThis is bar 1.2.0\nThis is baz 1.1.1\n", "")
+            assert process_output([bin_dir / "foo-1.3.2"]) == (
+                "This is foo 1.3.2\nThis is bar 1.2.0\nThis is baz 1.1.0\n", "")
 
             # Uninstall foo 1.3.0
             payne.uninstall("foo", "1.3.0")
