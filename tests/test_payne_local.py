084fa310c0f1e358ecbd95d576cdfc7dfc45856d
Martin Herrmann <martin@famherrmann.de>
2025-02-08 20:09:33 +0100

    Refactor and add more tests

--- tests/test_payne_local.py
+++ tests/test_payne_local.py
@@ -1,50 +1,84 @@
 import os
 from pathlib import Path
-import subprocess
 from tempfile import TemporaryDirectory
-
-import pytest
 
 from payne import Payne
 
+# noinspection PyUnresolvedReferences
 from fixtures.index_server import index_server
 from dirs import test_data
-from util import child_names
+from util import child_names, process_output
 
 
 class TestPayneLocal:
-    def test_install_from_local_free(self):
+    @staticmethod
+    def assert_app_valid(apps_dir: Path, name: str, version: str):
+        assert (apps_dir / name)                                .is_dir()   # app group dir
+        assert (apps_dir / name / version)                      .is_dir()   # app dir
+        assert (apps_dir / name / version / "payne_app.json")   .is_file()  # metadata
+        assert (apps_dir / name / version / name)               .is_dir()   # venv
+        assert (apps_dir / name / version / name / "pyvenv.cfg").is_file()  # venv metadata
+
+    @staticmethod
+    def installed_apps(app_dir: Path):
+        return {name: child_names(app_dir / name)
+                for name in child_names(app_dir)}
+
+    @staticmethod
+    def installed_scripts(bin_dir: Path):
+        return child_names(bin_dir)
+
+    def test_install_from_local_uninstall(self):
         with TemporaryDirectory() as temp_dir:
             temp_dir = Path(temp_dir)
             apps_dir = temp_dir / "apps"
             bin_dir = temp_dir / "bin"
 
             payne = Payne(apps_dir, bin_dir)
-
             # TODO this variable should be independent of uv
             os.environ["UV_INDEX"] = "payne_test_data=http://localhost:8000/payne_test_data"
+
+            # Install foo 1.3.0
             payne.install_from_local(test_data / "foo-1.3.0")
+            assert self.installed_apps(apps_dir) == {"foo": ["1.3.0"]}
+            assert self.installed_scripts(bin_dir) == ["foo-1.3.0.exe"]  # TODO Windows specific (all instances)
+            self.assert_app_valid(apps_dir, "foo", "1.3.0")
 
-            assert child_names(apps_dir) == ["foo"]
-            assert child_names(apps_dir / "foo") == ["1.3.0"]
-            assert (apps_dir / "foo" / "1.3.0" / "payne_app.json").is_file()
-            assert (apps_dir / "foo" / "1.3.0" / "foo").is_dir()
+            # Install foo 1.3.1
+            payne.install_from_local(test_data / "foo-1.3.1")
+            assert self.installed_apps(apps_dir) == {"foo": ["1.3.0", "1.3.1"]}
+            assert self.installed_scripts(bin_dir) == ["foo-1.3.0.exe", "foo-1.3.1.exe"]
+            self.assert_app_valid(apps_dir, "foo", "1.3.0")
+            self.assert_app_valid(apps_dir, "foo", "1.3.1")
 
-            assert child_names(bin_dir) == ["foo-1.3.0.exe"]  # TODO windows-specific
+            # Install foo 1.3.2
+            payne.install_from_local(test_data / "foo-1.3.2")
+            assert self.installed_apps(apps_dir) == {"foo": ["1.3.0", "1.3.1", "1.3.2"]}
+            assert self.installed_scripts(bin_dir) == ["foo-1.3.0.exe", "foo-1.3.1.exe", "foo-1.3.2.exe"]
+            self.assert_app_valid(apps_dir, "foo", "1.3.0")
+            self.assert_app_valid(apps_dir, "foo", "1.3.1")
+            self.assert_app_valid(apps_dir, "foo", "1.3.2")
 
-            # Run the script
-            try:
-                result = subprocess.run(
-                    [bin_dir / "foo-1.3.0.exe"],
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE,
-                    encoding="utf-8",
-                    universal_newlines=True,
-                    check=True,
-                )
-                assert result.stdout == "This is foo 1.3.0\nThis is bar 1.2.1\nThis is baz 1.1.1\n"
-                assert result.stderr == ""
-            except subprocess.CalledProcessError as e:
-                print(e.stdout)
-                print(e.stderr)  # TODO to stderr? (all instances)
-                raise
+            # Run the scripts
+            assert process_output([bin_dir / "foo-1.3.0.exe"]) == (
+                "This is foo 1.3.0\nThis is bar 1.2.1\nThis is baz 1.1.1\n", "")
+            assert process_output([bin_dir / "foo-1.3.1.exe"]) == (
+                "This is foo 1.3.1\nThis is bar 1.2.0\nThis is baz 1.1.1\n", "")
+
+            # Uninstall foo 1.3.0
+            payne.uninstall("foo", "1.3.0")
+            assert self.installed_apps(apps_dir) == {"foo": ["1.3.1", "1.3.2"]}
+            assert self.installed_scripts(bin_dir) == ["foo-1.3.1.exe", "foo-1.3.2.exe"]
+            self.assert_app_valid(apps_dir, "foo", "1.3.1")
+            self.assert_app_valid(apps_dir, "foo", "1.3.2")
+
+            # Uninstall foo 1.3.1
+            payne.uninstall("foo", "1.3.1")
+            assert self.installed_apps(apps_dir) == {"foo": ["1.3.2"]}
+            assert self.installed_scripts(bin_dir) == ["foo-1.3.2.exe"]
+            self.assert_app_valid(apps_dir, "foo", "1.3.2")
+
+            # Uninstall foo 1.3.2
+            payne.uninstall("foo", "1.3.2")
+            assert self.installed_apps(apps_dir) == {}
+            assert self.installed_scripts(bin_dir) == []
--- tests/util.py
+++ tests/util.py
@@ -1,5 +1,24 @@
 from pathlib import Path
+import subprocess
 
 
 def child_names(directory: Path) -> list[str]:
     return [child.name for child in directory.iterdir()]
+
+
+def process_output(args: list) -> tuple[str, str]:
+    # Run the script
+    try:
+        result = subprocess.run(
+            args,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            encoding="utf-8",
+            universal_newlines=True,
+            check=True,
+        )
+        return result.stdout, result.stderr
+    except subprocess.CalledProcessError as e:
+        print(e.stdout)
+        print(e.stderr)  # TODO to stderr? (all instances)
+        raise
